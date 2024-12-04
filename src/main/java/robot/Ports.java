02efab06bd6308a61dc58475627ef5ca79aa8e89
KysonNg <grimghostthen@gmail.com>
2024-12-03 22:45:20 -0500

    Commit it

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -3,14 +3,14 @@ package robot;
 public final class Ports {
   public static final class OI {
     public static final int OPERATOR = 0;
-    public static final int DRIVER = 1;
+    public static final int DRIVER = 2;
   }//I think we can delete the operator and use 0, but IDK
 
 public static final class Drive {
-  public static final int RIGHT_LEADER = 2;
-  public static final int RIGHT_FOLLOWER = 3;
-  public static final int LEFT_LEADER = 4;
-  public static final int LEFT_FOLLOWER = 5;
-  public static final int GYRO_CHANNEL = 6;
+  public static final int RIGHT_LEADER = 3;
+  public static final int RIGHT_FOLLOWER = 4;
+  public static final int LEFT_LEADER = 5;
+  public static final int LEFT_FOLLOWER = 6;
+  public static final int GYRO_CHANNEL = 1;
   }
 }
